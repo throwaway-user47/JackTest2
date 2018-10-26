c04c6496825bc2c47e3966cb4084e0280826bdad
Jack Dolabany <jdolabany@ngpvan.com>
2018-10-26 11:53:53 -0400

    test

--- WebApplication1/Powershell/powershell.ps1
+++ WebApplication1/Powershell/powershell.ps1
@@ -8,7 +8,7 @@ Import-Module posh-git
 $filePath = "$PSScriptRoot\..\TextFile1.txt"
 Add-Content $filePath "Date: $(Get-Date -Format g)"
 git status
-git add `.
+git add *
 git status
 git commit -m "test"
 #git push
