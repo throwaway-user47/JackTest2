5be50ab0690c9e0534c405c5ccffb2000e82ae23
Jack Dolabany <jdolabany@ngpvan.com>
2018-10-26 12:19:51 -0400

    updated powershell script

--- WebApplication1/Powershell/powershell.ps1
+++ WebApplication1/Powershell/powershell.ps1
@@ -1,14 +1,16 @@
 ﻿Set-Location $PSScriptRoot
 
-PowerShellGet\Install-Module posh-git -Scope CurrentUser
+#PowerShellGet\Install-Module posh-git -Scope CurrentUser
 
-Import-Module posh-git
+#Import-Module posh-git
 
 
 $filePath = "$PSScriptRoot\..\TextFile1.txt"
 Add-Content $filePath "Date: $(Get-Date -Format g)"
-git status
-git add *
+
+#git status
+git add ../TextFile1.txt
+
 git status
 git commit -m "test"
-#git push
+git push
